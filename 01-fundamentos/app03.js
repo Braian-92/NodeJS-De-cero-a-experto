267513a1766988ea25d6a1030543d25b68f501e6
Braian-92 <zamudiobraianhernan@gmail.com>
2024-05-15 21:13:20 -0300

    orden de callbacks

--- 01-fundamentos/app03.js
+++ 01-fundamentos/app03.js
@@ -5,8 +5,12 @@ const content = fs.readFileSync('README.md', 'utf-8')
 const words = content.split(' ')
 
 const reactWordsCount = words.filter(
-  (word) => word.toLowerCase() === 'react'
+  // (word) => word.toLowerCase() === 'react'
+  (word) => word.toLowerCase().includes('react')
 ).length
+
+const reactWordsCount2 = content.match(/react/gi ?? []).length
 
 console.log('🚀 ~ wordCount:', words.length)
 console.log('🚀 ~ reactWordsCount:', reactWordsCount)
+console.log("🚀 ~ reactWordsCount2:", reactWordsCount2)
