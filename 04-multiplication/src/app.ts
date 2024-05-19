9ec8cbb1a3e6ab7883fb886310ef2831182c4ba1
Braian-92 <zamudiobraianhernan@gmail.com>
2024-05-19 18:04:18 -0300

    generar archivos en directorio con creación de salida forzada

--- 04-multiplication/src/app.ts
+++ 04-multiplication/src/app.ts
@@ -1,2 +1,23 @@
-const message = "Hola Mundo"
-console.log(message)
+import fs, { mkdirSync } from 'fs'
+
+let outputMessage = ''
+const base = 6
+const headerMessage = `
+=======================================
+             Tabla del ${base}
+=======================================
+
+`
+
+for (let i = 0; i <= 10; i++) {
+  outputMessage += `${base} x ${i} = ${base * i}\n`
+}
+
+outputMessage = headerMessage + outputMessage
+
+console.log(outputMessage)
+
+const outputPath = 'outputs'
+
+fs.mkdirSync(outputPath, { recursive: true })
+fs.writeFileSync(`${outputPath}/tabla-${base}.log`, outputMessage)
