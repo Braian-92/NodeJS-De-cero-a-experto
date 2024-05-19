b79c2a3098dc60a235203330508870ea79ce9804
Braian-92 <zamudiobraianhernan@gmail.com>
2024-05-19 13:39:23 -0300

    fin pruebas en callbacks

--- 02-bases_ts_testing/test/js-fundation/03-callbacks.test.ts
+++ 02-bases_ts_testing/test/js-fundation/03-callbacks.test.ts
@@ -1,11 +1,24 @@
 import { getUserByID } from '../../src/js-fundation/03-callbacks'
 
 describe('js-fundation/03-callbacks', () => {
-  test('getUserByID should return an error if users does not exist', () => {
+  test('getUserByID should return an error if users does not exist', (done) => {
     const id = 10
     getUserByID(id, (err, user) => {
       expect( err ).toBe(`User not found with id ${id}`)
       expect( user ).toBeUndefined()
+      done()
+    })
+  })
+
+  test('getUserByID should return Braian Zamudio', (done) => {
+    const id = 1
+    getUserByID(id, (err, user) => {
+      expect( err ).toBeUndefined()
+      expect( user ).toEqual({
+        id: 1,
+        name: 'Braian Zamudio'
+      })
+      done()
     })
   })
 })
