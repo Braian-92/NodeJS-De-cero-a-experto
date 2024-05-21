80b9c9427d58074bead1e58017642d5c1e1caf6f
Braian-92 <zamudiobraianhernan@gmail.com>
2024-05-21 00:06:12 -0300

    refactorizado funcional completo

--- 04-multiplication/src/presentation/server-app.ts
+++ 04-multiplication/src/presentation/server-app.ts
@@ -1,4 +1,5 @@
-import { CreateTable } from "../domain/uses-cases/create-table.use-case"
+import { CreateTable } from '../domain/uses-cases/create-table.use-case'
+import { SaveFile } from '../domain/uses-cases/save-file.use-case'
 
 interface RunOptions {
   base: number
@@ -10,7 +11,18 @@ export class ServerApp {
   static run({ base, limit, showTable }: RunOptions) {
     console.log('Server running...')
 
-    const table = new CreateTable().execute({base, limit})
-    if(showTable) console.log(table)
+    const table = new CreateTable().execute({ base, limit })
+    const wasCreated = new SaveFile().execute({
+      fileContent: table,
+      fileDestination: `outputs/r_table-${base}`
+    })
+
+    if (showTable) {
+      console.log(table)
+    }
+
+    wasCreated
+      ? console.log('File created!')
+      : console.error('File not created!')
   }
 }
