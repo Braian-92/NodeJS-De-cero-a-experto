28396a8e261a5de7d1f537c8c4037f1a34cadeff
Braian-92 <zamudiobraianhernan@gmail.com>
2024-05-23 22:12:36 -0300

    inicializar config de testeing en jest

--- 04-multiplication/jest.config.js
+++
@@ -1,5 +0,0 @@
-preset: 'ts-jest',
-testEnvironment: "jest-environment-node",
-
-// Opcional - The paths to modules that run some code to configure or set up the testing environment before each test
-// setupFiles: ['dotenv/config'],
