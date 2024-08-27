446da80c9e866a86ae91b775ff1565732ff8075e
Braian-92 <zamudiobraianhernan@gmail.com>
2024-08-26 21:56:12 -0300

    test finales en args

--- 04-multiplication/src/config/plugins/args.plugin.test.ts
+++ 04-multiplication/src/config/plugins/args.plugin.test.ts
@@ -9,6 +9,13 @@ const runCommand = async( args: string[] ) => {
 
 
 describe('args.plugin.test.ts', () => {
+
+  const originalArgv = process.argv;
+  beforeEach( () => {
+    process.argv = originalArgv;
+    jest.resetModules();
+  })
+
   test('should return default values', async() => {
 
     const argv = await runCommand(['-b', '5']);
@@ -20,6 +27,26 @@ describe('args.plugin.test.ts', () => {
       s: false,
       n: 'multiplication-table',
       d: 'output',
+    }))
+  })
+
+  test('should return configuration with custom values', async() => {
+
+    const argv = await runCommand([
+      '-b','5',
+      '-l','20',
+      '-s',
+      '-n', 'custom-name',
+      '-d', 'custom-dir'
+    ]);
+
+    console.log(argv)
+    expect(argv).toEqual( expect.objectContaining({
+      b: 5,
+      l: 20,
+      s: true,
+      n: 'custom-name',
+      d: 'custom-dir',
     }))
   })
 })
