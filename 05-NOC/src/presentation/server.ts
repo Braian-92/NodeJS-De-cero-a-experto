90bc858992924c30efc1a61bbdeb8c90d4d9b0c2
Braian-92 <zamudiobraianhernan@gmail.com>
2024-08-28 23:28:01 -0300

    implementación check service

--- 05-NOC/src/presentation/server.ts
+++ 05-NOC/src/presentation/server.ts
@@ -1,3 +1,4 @@
+import { CheckService } from "../domain/use-cases/checks/check-service"; 
 import { CronService } from "./cron/cron-service"
 export class Server {
   public static start() {
@@ -8,6 +9,8 @@ export class Server {
       () => {
         const date = new Date();
         console.log('5 seconds', date);
+
+        new CheckService().execute('https://google.com')
       }
     )
   }
