0f65620594163812a69ad5e2efcd9e8bba047ef5
Braian-92 <zamudiobraianhernan@gmail.com>
2024-09-04 00:38:01 -0300

    envio de mail integrado y funcional

--- 05-NOC/src/presentation/server.ts
+++ 05-NOC/src/presentation/server.ts
@@ -2,6 +2,7 @@ import { CheckService } from "../domain/use-cases/checks/check-service";
 import { FileSystemDatasource } from "../infrastructure/datasources/file-system.datasource";
 import { LogRepositoryImp } from "../infrastructure/repositories/log.repository.imp";
 import { CronService } from "./cron/cron-service"
+import { EmailService } from "./email/email.service";
 
 const fileSistemLogRepository = new LogRepositoryImp(
   new FileSystemDatasource()
@@ -10,20 +11,30 @@ export class Server {
   public static start() {
     console.log('Server started...')
 
-    CronService.createJob(
-      '*/5 * * * * *',
-      () => {
-        const date = new Date();
-        console.log('5 seconds', date);
+    const emailService = new EmailService();
+    emailService.sendMail({
+      to: 'nattecheira@gmail.com',
+      subject: 'Logs de sistema',
+      htmlBody: `
+      <h3>Logs de sistema - NOC</h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+      `
+    })
 
-        const url = 'https://google.com';
+    // CronService.createJob(
+    //   '*/5 * * * * *',
+    //   () => {
+    //     const date = new Date();
+    //     console.log('5 seconds', date);
 
-        new CheckService(
-          fileSistemLogRepository,
-          () => console.log(`${url} is ok`),
-          ( error ) => console.log(error)
-        ).execute(url)
-      }
-    )
+    //     const url = 'https://google.com';
+
+    //     new CheckService(
+    //       fileSistemLogRepository,
+    //       () => console.log(`${url} is ok`),
+    //       ( error ) => console.log(error)
+    //     ).execute(url)
+    //   }
+    // )
   }
 }
