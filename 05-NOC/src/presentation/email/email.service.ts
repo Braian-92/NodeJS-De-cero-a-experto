20cd0cd2db5591848bdbc19125332b6101495a7b
Braian-92 <zamudiobraianhernan@gmail.com>
2024-09-04 00:29:19 -0300

    practica de envio de mail

--- 05-NOC/src/presentation/email/email.service.ts
+++ 05-NOC/src/presentation/email/email.service.ts
@@ -1,5 +1,42 @@
 import nodemailer from 'nodemailer';
+import { envs } from '../../config/plugins/envs.plugins';
+
+interface SendMailOptions {
+  to: string;
+  subject: string;
+  htmlBody: string;
+  //todo: attachments:
+}
+
+//todo: attachments:
 
 export class EmailService {
-  private transporter = nodemailer
+  private transporter = nodemailer.createTransport({
+    service: envs.MAILER_SERVICE,
+    auth: {
+      user: envs.MAILER_EMAIL,
+      pass: envs.MAILER_SECRET_KEY,
+    }
+  });
+
+
+  async sendMail(options: SendMailOptions):Promise<boolean> {
+
+    const { to, subject, htmlBody } = options;
+
+    try {
+
+      const sentInformation = await this.transporter.sendMail({
+        to: to,
+        subject: subject,
+        html: htmlBody
+      })
+
+      console.log(sentInformation);
+
+      return true;
+    } catch (error) {
+      return false;
+    }
+  }
 }
