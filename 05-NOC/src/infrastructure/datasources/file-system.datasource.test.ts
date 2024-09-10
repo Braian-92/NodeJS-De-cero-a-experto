73305fae012f194a98f1498172d30675c08bfa24
Braian-92 <zamudiobraianhernan@gmail.com>
2024-09-10 00:31:21 -0300

    fin test filesystem

--- 05-NOC/src/infrastructure/datasources/file-system.datasource.test.ts
+++ 05-NOC/src/infrastructure/datasources/file-system.datasource.test.ts
@@ -8,12 +8,12 @@ describe('FileSystemDatasource', () => {
 
   console.log({ logPath })
 
-  beforeEach(() => {
-    fs.rmSync(logPath, {
-      recursive: true,
-      force: true
-    })
-  })
+  // beforeEach( () => {
+  //   fs.rmSync(logPath, {
+  //     recursive: true,
+  //     force: true
+  //   })
+  // })
 
   test('should create log file if they do not exist', () => {
     new FileSystemDatasource()
@@ -74,4 +74,65 @@ describe('FileSystemDatasource', () => {
     expect(highLogs).toContain(JSON.stringify(log))
 
   })
+
+  // test('should return all logs', async() =>  {
+    
+
+  //   const LogDatasource = new FileSystemDatasource(); 
+
+  //   const lowLog = new LogEntity({
+  //     message: 'low-log',
+  //     level: LogSeverityLevel.low,
+  //     origin: 'low'
+  //   })
+
+  //   const mediumLog = new LogEntity({
+  //     message: 'medium-log',
+  //     level: LogSeverityLevel.medium,
+  //     origin: 'medium'
+  //   })
+
+  //   const highLog = new LogEntity({
+  //     message: 'high-log',
+  //     level: LogSeverityLevel.high,
+  //     origin: 'high'
+  //   })
+
+  //   await LogDatasource.saveLog(lowLog)
+  //   await LogDatasource.saveLog(mediumLog)
+  //   await LogDatasource.saveLog(highLog)
+
+  //   const lowLogGet = await LogDatasource.getLogs(LogSeverityLevel.low)
+  //   const mediumLogGet = await LogDatasource.getLogs(LogSeverityLevel.medium)
+  //   const highLogGet = await LogDatasource.getLogs(LogSeverityLevel.high)
+
+  //   expect(lowLogGet).toEqual( expect.arrayContaining([lowLogGet, mediumLogGet, highLogGet]) )
+  //   expect(mediumLogGet).toEqual( expect.arrayContaining([mediumLogGet]) )
+  //   expect(highLogGet).toEqual( expect.arrayContaining([highLogGet]) )
+
+  // })
+
+  test('should not throw an error if path exist', () => {
+    new FileSystemDatasource();
+    new FileSystemDatasource();
+    expect(true).toBeTruthy();
+  })
+
+  test('should throw an error if severity level is not defined', async() => {
+
+    const logDataSource = new FileSystemDatasource();
+    const customSeverityLevel = 'SUPER_MEGA_HIGH' as LogSeverityLevel;
+
+    try {
+      await logDataSource.getLogs(customSeverityLevel)
+      expect(true).toBeFalsy()
+    } catch (error) {
+      const errorString = `${ error }`
+      console.log( errorString );
+      expect(error).toContain(`${customSeverityLevel} not implemented`)
+    }
+
+  })
+
+
 })
