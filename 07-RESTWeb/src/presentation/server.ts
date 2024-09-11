db6a4690ac9c03553fa28c3beee12f60d6a11c26
Braian-92 <zamudiobraianhernan@gmail.com>
2024-09-11 01:30:51 -0300

    reload router

--- 07-RESTWeb/src/presentation/server.ts
+++ 07-RESTWeb/src/presentation/server.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import path from 'path';
 
 export class Server {
 
@@ -10,6 +11,14 @@ export class Server {
 
     //! Public folder
     this.app.use( express.static( 'public' ) )
+
+    this.app.get('*', (req, res) => {
+      /* console.log(req.url);
+      res.send('Hola mundo!') */
+      const indexPath = path.join( __dirname + '../../../public/index.html' )
+      res.sendFile(indexPath)
+      return;
+    })
 
 
     this.app.listen(3000, () => {
