15c547289090e65cdbcde477f0d1357fc330d074
Braian-92 <zamudiobraianhernan@gmail.com>
2024-09-10 23:54:52 -0300

    respuesta de tipo json en 8080

--- 06-RESTWeb/src/app.ts
+++ 06-RESTWeb/src/app.ts
@@ -3,10 +3,19 @@ import http from 'http';
 const server = http.createServer((req, res) => {
   console.log(req.url)
 
-  res.writeHead(200, {'Content-Type': 'text/html'})
+  // res.writeHead(200, {'Content-Type': 'text/html'})
+  // res.write(`<h1>Hola mundo! ${req.url}</h1>`);
+  // res.end()
 
-  res.write('<h1>Hola mundo!</h1>');
-  res.end()
+  const data = {
+    name: 'braian',
+    age: 31,
+    city: 'Buenos Aires'
+  }
+
+  res.writeHead(200, { 'Content-type': 'aplication/json' })
+  res.end( JSON.stringify(data) )
+
 })
 
 
