2339bd39d67b4bb2be9d88ca0de4a6977a0fb084
Braian-92 <zamudiobraianhernan@gmail.com>
2024-09-11 00:08:42 -0300

    pagina con interpretación de js y css importando un html existente

--- 06-RESTWeb/src/app.ts
+++ 06-RESTWeb/src/app.ts
@@ -1,4 +1,5 @@
 import http from 'http';
+import fs from 'fs';
 
 const server = http.createServer((req, res) => {
   console.log(req.url)
@@ -7,15 +8,33 @@ const server = http.createServer((req, res) => {
   // res.write(`<h1>Hola mundo! ${req.url}</h1>`);
   // res.end()
 
-  const data = {
-    name: 'braian',
-    age: 31,
-    city: 'Buenos Aires'
+  // const data = {
+  //   name: 'braian',
+  //   age: 31,
+  //   city: 'Buenos Aires'
+  // }
+  // res.writeHead(200, { 'Content-type': 'aplication/json' })
+  // res.end( JSON.stringify(data) )
+
+  if(req.url === '/'){
+    const htmlFile = fs.readFileSync('./public/index.html', 'utf-8')
+    res.writeHead(200, { 'Content-type': 'text/html' })
+    res.end( htmlFile )
+    return;
+  }
+  // else{
+  //   res.writeHead(404, { 'Content-type': 'text/html' })
+  //   res.end()
+  // }
+
+  if(req.url?.includes('.js')){
+    res.writeHead(200, { 'Content-type': 'aplication/javascript' })
+  }else if(req.url?.includes('.css')){
+    res.writeHead(200, { 'Content-type': 'text/css' })
   }
 
-  res.writeHead(200, { 'Content-type': 'aplication/json' })
-  res.end( JSON.stringify(data) )
-
+  const responseContent = fs.readFileSync(`./public${ req.url }`, 'utf-8')
+  res.end( responseContent )
 })
 
 
