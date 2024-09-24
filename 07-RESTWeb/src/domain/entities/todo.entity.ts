99822e39eb3c86efb79e062c46934df2f71b75c8
Braian-92 <zamudiobraianhernan@gmail.com>
2024-09-23 22:54:05 -0300

    conversor de todoEntity

--- 07-RESTWeb/src/domain/entities/todo.entity.ts
+++ 07-RESTWeb/src/domain/entities/todo.entity.ts
@@ -13,4 +13,21 @@ export class TodoEntity {
     // doble negación si tiene un valor es true
     return !!this.coompleteAt;
   }
+
+  public static fromObject(object: {[key: string]: any} ){
+    const { id, text, completeAt } = object;
+    if( !id ) throw 'Id is required';
+    if( !text ) throw 'Text is required';
+
+    let newCompleteAt;
+    if( completeAt ) {
+      newCompleteAt = new Date(completeAt);
+      if( isNaN( newCompleteAt.getTime() ) ){
+        throw 'CompleteAt is not a valid date';
+      }
+    }
+
+    new TodoEntity(id, text, completeAt)
+    
+  }
 }
