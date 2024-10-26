20d8eb0f95ecc908dc5512b3d725321a4a1c887d
Braian-92 <zamudiobraianhernan@gmail.com>
2024-10-26 17:30:30 -0300

    centralizando errores

--- 07-RESTWeb/src/presentation/todos/controller.ts
+++ 07-RESTWeb/src/presentation/todos/controller.ts
@@ -1,5 +1,4 @@
 import { Request, Response } from 'express';
-import { prisma } from '../../data/postgres';
 import { CreateTodoDto, UpdateTodoDto } from '../../domain/dtos';
 import { CreateTodo, CustomError, DeleteTodo, GetTodo, GetTodos, TodoRepository, UpdateTodo } from '../../domain';
 
@@ -11,13 +10,24 @@ export class TodosController {
     private readonly todoRepository: TodoRepository,
   ) { }
 
+  // Response es de express
+  private handleError = ( res: Response, error: unknown) => {
+    if(error instanceof CustomError){
+      res.status(error.statusCode).json({error: error.message});
+      return;
+    }
+
+    // ! granar logs
+    res.status(500).json({error: 'Internal server error - check logs'})
+  }
+
 
   public getTodos = ( req: Request, res: Response ) => {
 
     new GetTodos( this.todoRepository )
     .execute()
       .then( todos => res.json( todos ) )
-      .catch( error => res.status( 400 ).json( { error } ) );
+      .catch( error => this.handleError(res, error) );
 
   };
 
@@ -27,7 +37,7 @@ export class TodosController {
     new GetTodo( this.todoRepository )
       .execute( id )
       .then( todo => res.json( todo ) )
-      .catch( ( error:CustomError ) => res.status( error.statusCode ).json( { error : error.message } ) );
+      .catch( error => this.handleError(res, error) );
 
   };
 
@@ -38,7 +48,7 @@ export class TodosController {
     new CreateTodo( this.todoRepository )
       .execute( createTodoDto! )
       .then( todo => res.status(201).json( todo ) )
-      .catch( error => res.status( 400 ).json( { error } ) );
+      .catch( error => this.handleError(res, error) );
 
 
   };
@@ -52,7 +62,7 @@ export class TodosController {
     new UpdateTodo( this.todoRepository )
     .execute( updateTodoDto! )
       .then( todo => res.json( todo ) )
-      .catch( error => res.status( 400 ).json( { error } ) );
+      .catch( error => this.handleError(res, error) );
 
   };
 
@@ -63,8 +73,7 @@ export class TodosController {
     new DeleteTodo( this.todoRepository )
       .execute( id )
       .then( todo => res.json( todo ) )
-      .catch( error => res.status( 400 ).json( { error } ) );
-
+      .catch( error => this.handleError(res, error) );
   };
 
 
