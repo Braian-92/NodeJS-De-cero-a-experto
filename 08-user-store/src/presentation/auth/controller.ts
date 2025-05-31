f3621954122dc59bf05107c958a7cf6df8ee1381
Braian-92 <zamudiobraianhernan@gmail.com>
2025-05-31 18:03:32 -0300

    delegar servicio de creación de usuarios

--- 08-user-store/src/presentation/auth/controller.ts
+++ 08-user-store/src/presentation/auth/controller.ts
@@ -1,12 +1,15 @@
 import { Request, Response } from "express"
 import { RegisterUserDto } from "../../domain"
+import { AuthService } from "../services/auth.service";
 
 
 
 
 export class AuthController {
   // DI
-  constructor () {}
+  constructor (
+    public readonly authService: AuthService
+  ) {}
 
 
   registerUser = (req: Request, res: Response) => {
@@ -14,7 +17,10 @@ export class AuthController {
     
     if (error) return res.status(400).json({ error });
     
-    return res.json(registerDto);
+    // return res.json(registerDto);
+    this.authService.registerUser(registerDto!)
+      .then(user => res.json(user))
+      .catch(error => res.status(400).json({ error }))
   }
 
   loginUser = (req: Request, res: Response) => {
---
+++ 08-user-store/src/presentation/services/auth.service.ts
@@ -0,0 +1,18 @@
+import { UserModel } from "../../data";
+import { CustomError, RegisterUserDto } from "../../domain";
+
+
+
+
+export class AuthService {
+
+  // DI
+  constructor () {}
+
+  public async registerUser (registerUserDto: RegisterUserDto ) {
+    const existUser = await UserModel.findOne({ email: registerUserDto.email });
+    if (existUser) return CustomError.badRequest('User already exists');
+
+    return 'todo ok!';
+  }
+}
