75b8f9e9c3ba246f6fe07f4b39ebdff4ea819a80
Braian-92 <zamudiobraianhernan@gmail.com>
2025-06-03 23:43:47 -0300

    integrar semilla de cifrado para el JWT

--- 08-user-store/src/config/jwt.adapter.ts
+++ 08-user-store/src/config/jwt.adapter.ts
@@ -1,5 +1,7 @@
 import jwt from 'jsonwebtoken';
+import { envs } from './envs';
 
+const JWT_SEED = envs.JWT_SEED;
 
 export class JwtAdapter {
 
@@ -7,7 +9,7 @@ export class JwtAdapter {
 
   static async generateToken(payload: any, duration: string = '2h') {
     return new Promise((resolve) => {
-      jwt.sign(payload, "SEED", { expiresIn: duration as jwt.SignOptions['expiresIn'] }, (err, token) => {
+      jwt.sign(payload, JWT_SEED, { expiresIn: duration as jwt.SignOptions['expiresIn'] }, (err, token) => {
         if ( err ) return resolve(null);
         
         resolve(token);
