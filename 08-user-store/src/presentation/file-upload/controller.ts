a21189f6c119e8b2acb22471677ae1e1bc8be6cd
Braian-92 <zamudiobraianhernan@gmail.com>
2025-06-10 23:30:27 -0300

    guardar archivos en carpetas según el tipo

--- 08-user-store/src/presentation/file-upload/controller.ts
+++ 08-user-store/src/presentation/file-upload/controller.ts
@@ -22,7 +22,13 @@ export class FileUploadController {
 
   uploadFile = (req: Request, res: Response) => {
 
-    const files = req.files;
+    const type = req.params.type;
+    const validTypes = ['users', 'products', 'categories'];
+
+    if( !validTypes.includes( type )) {
+      return res.status(400)
+        .json({ error: `Invalid type, valid types: ${ validTypes.join(', ') }` });
+    }
 
     if( !req.files || Object.keys( req.files ).length === 0 ) {
       return res.status(400).json({ error: 'No files were selected.' });
@@ -30,7 +36,7 @@ export class FileUploadController {
 
     const file = req.files.file as UploadedFile;
 
-    this.fileUploadService.uploadSingle( file )
+    this.fileUploadService.uploadSingle( file, `uploads/${ type }` )
       .then( uploaded => res.json( uploaded ))
       .catch( error => this.handleError( error, res ));
 
