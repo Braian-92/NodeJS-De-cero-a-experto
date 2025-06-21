a56ed9a66a47a09dd8a6b051b1325df3ca68b53d
Braian-92 <zamudiobraianhernan@gmail.com>
2025-06-21 13:44:07 -0300

    autenticar peticiones de webhooks github con sha256

--- 09-github-webhooks/src/app.ts
+++ 09-github-webhooks/src/app.ts
@@ -1,6 +1,7 @@
 import express from "express"
 import { env } from "./config"
 import { GithubController } from "./presentation/github/controller"
+import { GithubSha256Middleware } from "./presentation/middlewares/github-sha256.middleware"
 
 
 ( () => {
@@ -13,6 +14,7 @@ function main() {
   const githubController = new GithubController();
   // Middleware para procesar JSON
   app.use(express.json())
+  app.use(GithubSha256Middleware.verifyGithubSignature)
 
   // app.post('/api/github', (req, res) => {
   //   console.log(req.body);
