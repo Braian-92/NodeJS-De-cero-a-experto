098040e763226d60e4a7c34dabb48bc6eb71108d
Braian-92 <zamudiobraianhernan@gmail.com>
2025-06-22 13:52:46 -0300

    base singleton de websocket

---
+++ 13-websocket-server/src/presentation/service/wss.service.ts
@@ -0,0 +1,46 @@
+import { Server } from "http";
+import { WebSocketServer, WebSocket } from "ws";
+
+export interface Options {
+  server: Server;
+  path?: any;
+}
+
+
+export class WssService {
+  private static _instance: WssService;
+  private wss: WebSocketServer;
+
+
+  private constructor(options: Options) {
+    const { server, path = '/ws' } = options; //! localhost:3000/ws
+
+    this.wss = new WebSocketServer({ server, path });
+    this.start();
+  }
+
+  static getInstance(): WssService {
+    if (!WssService._instance) {
+      throw 'WssService not initialized';
+    }
+
+    return WssService._instance;
+  }
+
+  static initWss(options: Options) {
+    WssService._instance = new WssService(options);
+  }
+
+  public start() {
+    this.wss.on('connection', (ws: WebSocket) => {
+      console.log('Client connected');
+
+      ws.on('close', () => {
+        console.log('Client disconnected');
+      });
+
+    });
+  }
+  
+}
+
